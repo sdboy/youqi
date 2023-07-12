ddd798865c52f64b2bacea59ab352d3312ffae7e
sdboy <jiaoyiguanggo@gmial.com>
2023-07-13 04:02:53 +0800

    init

---
+++ src/main/java/net/eyeb/youqi/YouqiApplication.java
@@ -0,0 +1,13 @@
+package net.eyeb.youqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouqiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YouqiApplication.class, args);
+  }
+
+}
