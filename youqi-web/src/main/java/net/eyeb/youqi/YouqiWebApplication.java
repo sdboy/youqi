310bfcc0932f5a469397d6291a885385311bdf1e
sdboy <jiaoyiguanggo@gmial.com>
2023-07-13 17:20:37 +0800

    delete useless file

--- youqi-web/src/main/java/net/eyeb/youqi/YouqiWebApplication.java
+++ youqi-web/src/main/java/net/eyeb/youqi/YouqiWebApplication.java
@@ -1,9 +1,11 @@
 package net.eyeb.youqi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"net.eyeb.youqi.db1", "net.eyeb.youqi.db2"})
 public class YouqiWebApplication {
 
   public static void main(String[] args) {
